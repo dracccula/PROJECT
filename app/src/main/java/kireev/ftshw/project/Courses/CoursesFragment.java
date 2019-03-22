58058937357e2b228d3e108d0c98979b7c9e9606
dracccula <drcccla@gmail.com>
2019-03-22 19:47:32 +0300

    fixed getting badges in not-ui thread

--- app/src/main/java/kireev/ftshw/project/Courses/CoursesFragment.java
+++ app/src/main/java/kireev/ftshw/project/Courses/CoursesFragment.java
@@ -1,11 +1,16 @@
 package kireev.ftshw.project.Courses;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import kireev.ftshw.project.MainActivity;
 import kireev.ftshw.project.R;
@@ -22,6 +27,7 @@ import static kireev.ftshw.project.Courses.GradesFragment.viewAvatarThree;
 public class CoursesFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
 
     private SwipeRefreshLayout mSwipeRefreshLayout;
+
     private LooperThread looperThread;
 
     public CoursesFragment() {
@@ -37,8 +43,8 @@ public class CoursesFragment extends Fragment implements SwipeRefreshLayout.OnRe
         View v = inflater.inflate(R.layout.fragment_courses, container, false);
         mSwipeRefreshLayout = v.findViewById(R.id.swipe_container);
         mSwipeRefreshLayout.setOnRefreshListener(this);
-        looperThread = new LooperThread();
-        looperThread.start();
+//        looperThread = new LooperThread();
+//        looperThread.start();
         return v;
     }
 
@@ -48,24 +54,45 @@ public class CoursesFragment extends Fragment implements SwipeRefreshLayout.OnRe
     }
 
     private void postRunnable(final int id1, final int id2, final int id3) {
-        looperThread.post(new Runnable() {
-            @Override
-            public void run() {
-                tryToSleepForTwoSecondsInNewThread(id1, id2, id3);
-            }
-        });
+        tryToSleepForTwoSecondsInNewThread(id1, id2, id3);
+//        looperThread.post(new Runnable() {
+//            @Override
+//            public void run() {
+//            }
+//        });
     }
 
     private void tryToSleepForTwoSecondsInNewThread(final int id1, final int id2, final int id3) {
+        final Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == 42) {
+                    List<String> badges = (List<String>) msg.obj;
+                    pointsBadge1.setText(badges.get(0));
+                    pointsBadge2.setText(badges.get(1));
+                    pointsBadge3.setText(badges.get(2));
+                    viewAvatarOne.setBackgroundColor(SetRandom.SetRandomColor());
+                    viewAvatarTwo.setBackgroundColor(SetRandom.SetRandomColor());
+                    viewAvatarThree.setBackgroundColor(SetRandom.SetRandomColor());
+                    mSwipeRefreshLayout.setRefreshing(false);
+                }
+            }
+        };
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
                     Thread.sleep(2000);
-                    logMessageInUiThread(id1, id2, id3);
                 } catch (InterruptedException e) {
-                    logMessageInUiThread(id1, id2, id3);
+                    e.printStackTrace();
                 }
+                final List<String> messages = new ArrayList<>(3);
+                messages.add(String.valueOf(SetRandom.SetRandomInt()));
+                messages.add(String.valueOf(SetRandom.SetRandomInt()));
+                messages.add(String.valueOf(SetRandom.SetRandomInt()));
+                Message message = handler.obtainMessage(42, messages);
+                handler.sendMessage(message);
+//                logMessageInUiThread(id1, id2, id3);
             }
         }).start();
     }
